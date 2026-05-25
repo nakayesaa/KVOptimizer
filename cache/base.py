f45e5b9b744effb321e6d30d3b454b6d1838daff
nakayesaa <hajendraherlambang742@gmail.com>
2026-05-25 12:34:26 +0700

    add get abstrach method for subclass

--- cache/base.py
+++ cache/base.py
@@ -5,11 +5,24 @@ class BaseKVCache(ABC):
     "ABC for custom KV Cache implementations"
     def __init__(self, budget: int):
         self.budget = budget
+
     @abstractmethod
-    def update(self, key_state: torch.Tensor, value_state: torch.Tensor, layer_idx: int) -> tuple[torch.Tensor, torch.Tensor]:
+    def update(
+        self,
+        keyState: torch.Tensor,
+        valueState: torch.Tensor,
+        layerIndex: int,
+    ) -> tuple[torch.Tensor, torch.Tensor]:
         "Updates the cache with the new key and value states for a specific layer"
         pass
+
+    @abstractmethod
+    def get(self, layerIndex: int) -> tuple[torch.Tensor, torch.Tensor] | None:
+        "Retrieves the cached key and value states for a specific layer"
+        pass
+
     @abstractmethod
     def reset(self):
         "Resets or clears the cache state"
         pass
+
