88292ad0e1c87d1d7d0bef95c0aedd2158299481
nakayesaa <hajendraherlambang742@gmail.com>
2026-05-25 19:54:07 +0700

    implement update function to perform windows pruning kvcache

--- cache/streaming.py
+++ cache/streaming.py
@@ -15,7 +15,28 @@ class StreamingKVCache(BaseKVCache):
         valueState: torch.Tensor,
         layerIndex: int,
     ) -> tuple[torch.Tensor, torch.Tensor]:
-        pass
+        if layerIndex not in self.storeKeyCache:
+            self.storeKeyCache[layerIndex] = keyState
+            self.storeValueCache[layerIndex] = valueState
+        else:
+            self.storeKeyCache[layerIndex] = torch.cat(
+                [self.storeKeyCache[layerIndex], keyState], dim=-2
+            )
+            self.storeValueCache[layerIndex] = torch.cat(
+                [self.storeValueCache[layerIndex], valueState], dim=-2
+            )
+        currentLen = self.storeKeyCache[layerIndex].shape[-2]
+        if currentLen > self.budget:
+            sinkK = self.storeKeyCache[layerIndex][..., :self.keepSinkTokens, :]
+            sinkV = self.storeValueCache[layerIndex][..., :self.keepSinkTokens, :]
+
+            windowK = self.storeKeyCache[layerIndex][..., -self.holdWindowTokens:, :]
+            windowV = self.storeValueCache[layerIndex][..., -self.holdWindowTokens:, :]
+            
+            self.storeKeyCache[layerIndex] = torch.cat([sinkK, windowK], dim=-2)
+            self.storeValueCache[layerIndex] = torch.cat([sinkV, windowV], dim=-2)
+
+        return self.storeKeyCache[layerIndex], self.storeValueCache[layerIndex]
 
     def get(self, layerIndex: int) -> tuple[torch.Tensor, torch.Tensor] | None:
         if layerIndex not in self.storeKeyCache:
